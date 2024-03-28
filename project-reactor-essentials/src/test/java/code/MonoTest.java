c3b4432f2ea5434f8365ea40cba943612425f007
Hirak Bhattacharjee <bh.hirak@gmail.com>
2024-03-28 18:57:30 +0530

    subscribe() other variants

--- project-reactor-essentials/src/test/java/code/MonoTest.java
+++ project-reactor-essentials/src/test/java/code/MonoTest.java
@@ -2,6 +2,7 @@ package code;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -41,6 +42,8 @@ public class MonoTest {
         Mono<String> mono = Mono.just(name)
                 .map(s -> {throw new RuntimeException("Testing mono with error!");});
 
+        // Params 1: consumer – the consumer to invoke on each next signal
+        // 2: errorConsumer – the consumer to invoke on error signal
         mono.subscribe(s->log.info("Name: "+s), s->log.error("ERROR HAPPENED!"));
         mono.subscribe(s -> log.info("Name {}", s), Throwable::printStackTrace);
         log.info("--------------------------");
@@ -48,4 +51,52 @@ public class MonoTest {
                 .expectError()
                 .verify();
     }
+
+    @Test
+    public void monoSubscriberConsumerErrorComplete(){
+        String name = "William Suane";
+        Mono<String> mono = Mono.just(name).log()
+                .map(String::toUpperCase);
+
+        // Params 1: consumer – the consumer to invoke on each next signal
+        // 2: errorConsumer – the consumer to invoke on error signal
+        // 3: Runnable completeConsumer - the consumer to invoke on complete signal
+        mono.subscribe(s->log.info("Name: "+s),
+                s->log.error("ERROR HAPPENED!"),
+                () -> log.info("FINISHED!!"));
+
+        log.info("--------------------------");
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoSubscriberConsumerErrorCompleteSubscription(){
+        String name = "William Suane";
+        Mono<String> mono = Mono.just(name).log()
+                .map(String::toUpperCase);
+
+        // Params 1: Consumer consumer – the consumer to invoke on each next signal
+        // 2: Consumer errorConsumer – the consumer to invoke on error signal
+        // 3: Runnable completeConsumer - the consumer to invoke on complete signal
+        // 4: Consumer subscriptionConsumer - the consumer to invoke on subscribe signal
+        mono.subscribe(s->log.info("Name: "+s),
+                s->log.error("ERROR HAPPENED!"),
+                () -> log.info("FINISHED!!"),
+                Subscription::cancel);
+
+        log.info("--------------------------");
+        mono.subscribe(s->log.info("Name: "+s),
+                s->log.error("ERROR HAPPENED!"),
+                () -> log.info("FINISHED!!"),
+                subscription -> log.info("on subscribe: "+subscription.toString()));
+
+        log.info("--------------------------");
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
+    }
+
+
 }
