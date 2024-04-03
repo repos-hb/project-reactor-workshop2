44b4532c22928c1e27df78e90507913d46697e36
Hirak Bhattacharjee <bh.hirak@gmail.com>
2024-04-03 12:23:57 +0530

    error handling methods

---
+++ project-reactor-essentials/src/test/java/code/MonoDoOnErrorMethodsTest.java
@@ -0,0 +1,55 @@
+package code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class MonoDoOnErrorMethodsTest {
+
+    @Test
+    public void testDoOnError(){
+        Mono<Object> errorMono = Mono.error(new IllegalArgumentException("error from mono"))
+                .doOnError(throwable -> log.error(throwable.getMessage()))
+                .doOnNext(s->log.info("handling error from mono"))      // ignored
+                .log();
+
+        StepVerifier.create(errorMono)
+                .expectError(IllegalArgumentException.class)
+                .verify();
+    }
+
+    @Test
+    public void testOnErrorResume(){
+        String data = "new mono";
+        Mono<Object> errorMono = Mono.error(new IllegalArgumentException("error from mono"))
+                .onErrorResume(throwable -> {
+                    log.info("handling error from onErrorResume");
+                    return Mono.just(data);
+                })
+                .doOnError(throwable -> {       // ignored
+                    log.info("handling error from doOnError");
+                    log.error(throwable.getMessage());
+                })
+                .doOnNext(s->log.info("executing after handling error"))
+                .log();
+
+        StepVerifier.create(errorMono)
+                .expectNext(data)
+                .verifyComplete();
+    }
+
+    @Test
+    public void testOnErrorReturn(){
+        String data = "new mono";
+        Mono<Object> errorMono = Mono.error(new IllegalArgumentException("error from mono"))
+                .onErrorReturn(Mono.just(data))
+                .doOnNext(s->log.info("executing after handling error"))
+                .log();
+
+        StepVerifier.create(errorMono)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+}
